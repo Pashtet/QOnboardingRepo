a8563d7002e26ec123355955dcdc07ffecebac52
Pavel Trofimov <paveltrfimv@gmail.com>
2022-02-03 08:28:35 +0300

    Задача по главе 9 Числа через TO_CHAR с NLS параметрами

--- 9_number_to_char.sql
+++ 9_number_to_char.sql
@@ -1,5 +1,3 @@
---нужно изменить параметры сессии/бд
-alter session set nls_numeric_characters ='. ';
 BEGIN
-DBMS_OUTPUT.PUT_LINE(TO_CHAR(123456.789,'999G999D99'));
+DBMS_OUTPUT.PUT_LINE(TO_CHAR(123456.789,'999G999D99','NLS_NUMERIC_CHARACTERS = ''. '''));
 END;
