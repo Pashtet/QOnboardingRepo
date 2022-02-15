523d9460aa7695821deecc0d66fbd72a3748df6f
Pashtet <paveltrfimv@gmail.com>
2022-02-15 08:43:12 +0300

    refactoring with local procedure display instead of package procedure

--- nested_table.sql
+++ nested_table.sql
@@ -1,73 +1,11 @@
 -- работа с вложенной таблицей
 
 declare
-
-  n_t_test pt_work_with_nested_table.n_table := pt_work_with_nested_table.n_table();
-  i        PLS_INTEGER;
-  
-begin
-
-  DBMS_OUTPUT.PUT_LINE('Create nested table from 1 to 10.');
-
-  FOR i IN 1 .. 10 LOOP
-    n_t_test.EXTEND;
-    n_t_test(i) := i;
-  END LOOP;
-
-  pt_work_with_nested_table.display_direct_n_table(n_t_test);
-  pt_work_with_nested_table.display_reverse_n_table(n_t_test);
-
-  DBMS_OUTPUT.PUT_LINE('Display number of elements nested table: ' ||
-                       n_t_test.COUNT);
-                       
-  DBMS_OUTPUT.PUT_LINE('Delete first element in nested table!');
-  n_t_test.DELETE(n_t_test.FIRST);
-
-  pt_work_with_nested_table.display_direct_n_table(n_t_test);
-  
-  DBMS_OUTPUT.PUT_LINE('Is element 10 exist in nested table?!');
-  IF n_t_test.EXISTS(10) THEN
-    DBMS_OUTPUT.PUT_LINE('YES!');
-  ELSE
-    DBMS_OUTPUT.PUT_LINE('No!');
-  END IF;
-  
-    DBMS_OUTPUT.PUT_LINE('Delete last element in a_array!');
-  n_t_test.DELETE(n_t_test.LAST);
-  pt_work_with_nested_table.display_direct_n_table(n_t_test);
-  
-  DBMS_OUTPUT.PUT_LINE('Is element 10 still exist in nested table?!');
-  IF n_t_test.EXISTS(10) THEN
-    DBMS_OUTPUT.PUT_LINE('YES!');
-  ELSE
-    DBMS_OUTPUT.PUT_LINE('No!');
-  END IF;
-  
-  DBMS_OUTPUT.PUT_LINE('Display number of elements a_array: ' ||
-                       n_t_test.COUNT);
-
-end;
-
-
-create or replace package pt_work_with_nested_table is
-
-  -- Author  : P.TROFIMOV
-  -- Created : 11.02.2022 14:50:12
-  -- Purpose : Work with nested table
-
   TYPE n_table IS TABLE of PLS_INTEGER;
-
-  PROCEDURE display_direct_n_table(in_n_table IN n_table);
-  PROCEDURE display_reverse_n_table(in_n_table IN n_table);
-
-end pt_work_with_nested_table;
-
-
-create or replace package body pt_work_with_nested_table is
+  n_t_test n_table := n_table();
+  i        PLS_INTEGER;
 
   PROCEDURE display_direct_n_table(in_n_table IN n_table) IS
-    i PLS_INTEGER;
-  
   BEGIN
     DBMS_OUTPUT.PUT_LINE('Display direct nested table:');
     i := in_n_table.FIRST;
@@ -78,8 +16,6 @@ create or replace package body pt_work_with_nested_table is
   END display_direct_n_table;
 
   PROCEDURE display_reverse_n_table(in_n_table IN n_table) IS
-    i PLS_INTEGER;
-  
   BEGIN
     DBMS_OUTPUT.PUT_LINE('Display reverse nested table:');
     i := in_n_table.LAST;
@@ -87,5 +23,47 @@ create or replace package body pt_work_with_nested_table is
       DBMS_OUTPUT.PUT_LINE(in_n_table(i));
       i := in_n_table.PRIOR(i);
     END LOOP;
-END display_reverse_n_table;
-end pt_work_with_nested_table;
+  END display_reverse_n_table;
+
+begin
+
+  DBMS_OUTPUT.PUT_LINE('Create nested table from 1 to 10.');
+
+  FOR i IN 1 .. 10 LOOP
+    n_t_test.EXTEND;
+    n_t_test(i) := i;
+  END LOOP;
+
+  display_direct_n_table(n_t_test);
+  display_reverse_n_table(n_t_test);
+
+  DBMS_OUTPUT.PUT_LINE('Display number of elements nested table: ' ||
+                       n_t_test.COUNT);
+
+  DBMS_OUTPUT.PUT_LINE('Delete first element in nested table!');
+  n_t_test.DELETE(n_t_test.FIRST);
+
+  display_direct_n_table(n_t_test);
+
+  DBMS_OUTPUT.PUT_LINE('Is element 10 exist in nested table?!');
+  IF n_t_test.EXISTS(10) THEN
+    DBMS_OUTPUT.PUT_LINE('YES!');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('No!');
+  END IF;
+
+  DBMS_OUTPUT.PUT_LINE('Delete last element in a_array!');
+  n_t_test.DELETE(n_t_test.LAST);
+  display_direct_n_table(n_t_test);
+
+  DBMS_OUTPUT.PUT_LINE('Is element 10 still exist in nested table?!');
+  IF n_t_test.EXISTS(10) THEN
+    DBMS_OUTPUT.PUT_LINE('YES!');
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('No!');
+  END IF;
+
+  DBMS_OUTPUT.PUT_LINE('Display number of elements a_array: ' ||
+                       n_t_test.COUNT);
+
+end;
--- v_array.sql
+++ v_array.sql
@@ -1,9 +1,29 @@
--- Created on 14.02.2022 by P.TROFIMOV 
 -- работа с varray
 declare
-
-  v_array_test pt_work_with_v_array.v_array := pt_work_with_v_array.v_array();
+  TYPE v_array IS VARRAY(10) of PLS_INTEGER;
+  v_array_test v_array := v_array();
   i            PLS_INTEGER;
+
+  PROCEDURE display_direct_v_array(in_v_array IN v_array) IS
+  BEGIN
+    DBMS_OUTPUT.PUT_LINE('Display direct v_array');
+    i := in_v_array.FIRST;
+    WHILE (i IS NOT NULL) LOOP
+      DBMS_OUTPUT.PUT_LINE(in_v_array(i));
+      i := in_v_array.NEXT(i);
+    END LOOP;
+  END display_direct_v_array;
+
+  PROCEDURE display_reverse_v_array(in_v_array IN v_array) IS
+  BEGIN
+    DBMS_OUTPUT.PUT_LINE('Display reverse v_array');
+    i := in_v_array.LAST;
+    WHILE (i IS NOT NULL) LOOP
+      DBMS_OUTPUT.PUT_LINE(in_v_array(i));
+      i := in_v_array.PRIOR(i);
+    END LOOP;
+  END display_reverse_v_array;
+
 begin
 
   DBMS_OUTPUT.PUT_LINE('Create v_array from 1 to 10.');
@@ -13,8 +33,8 @@ begin
     v_array_test(i) := i;
   END LOOP;
 
-  pt_work_with_v_array.display_direct_v_array(v_array_test);
-  pt_work_with_v_array.display_reverse_v_array(v_array_test);
+  display_direct_v_array(v_array_test);
+ display_reverse_v_array(v_array_test);
 
   DBMS_OUTPUT.PUT_LINE('Display number of elements v_array: ' ||
                        v_array_test.COUNT);
@@ -37,7 +57,7 @@ begin
 
   DBMS_OUTPUT.PUT_LINE('TRIM last element in v_array');
   v_array_test.TRIM;
-  pt_work_with_v_array.display_direct_v_array(v_array_test);
+  display_direct_v_array(v_array_test);
 
   --добавить новый элемент можно только в конец с EXTEND, если не превысили количество возможных элементов
   DBMS_OUTPUT.PUT_LINE('Add last element 1 with EXTEND in v_array');
@@ -47,7 +67,7 @@ begin
   ELSE
     DBMS_OUTPUT.PUT_LINE('COUNT = LIMIT');
   END IF;
-  pt_work_with_v_array.display_direct_v_array(v_array_test);
+  display_direct_v_array(v_array_test);
 
   DBMS_OUTPUT.PUT_LINE('Try add element with element number > LIMIT in v_array');
   IF v_array_test.LIMIT > v_array_test.COUNT THEN
@@ -56,54 +76,6 @@ begin
   ELSE
     DBMS_OUTPUT.PUT_LINE('COUNT = LIMIT');
   END IF;
-  pt_work_with_v_array.display_direct_v_array(v_array_test);
+ display_direct_v_array(v_array_test);
 
 end;
-
-
-create or replace package pt_work_with_v_array is
-
-  -- Author  : P.TROFIMOV
-  -- Created : 14.02.2022 12:32:46
-  -- Purpose : work with varray
-
-  TYPE v_array IS VARRAY(10) of PLS_INTEGER;
-
-  PROCEDURE display_direct_v_array(in_v_array IN v_array);
-  PROCEDURE display_reverse_v_array(in_v_array IN v_array);
-
-end pt_work_with_v_array;
-
-create or replace package body pt_work_with_v_array is
-
-  PROCEDURE display_direct_v_array(in_v_array IN v_array) IS
-  
-    i PLS_INTEGER;
-  
-  BEGIN
-  
-    DBMS_OUTPUT.PUT_LINE('Display direct v_array');
-    i := in_v_array.FIRST;
-    WHILE (i IS NOT NULL) LOOP
-      DBMS_OUTPUT.PUT_LINE(in_v_array(i));
-      i := in_v_array.NEXT(i);
-    END LOOP;
-  
-  END display_direct_v_array;
-
-  PROCEDURE display_reverse_v_array(in_v_array IN v_array) IS
-  
-    i PLS_INTEGER;
-  
-  BEGIN
-  
-    DBMS_OUTPUT.PUT_LINE('Display reverse v_array');
-    i := in_v_array.LAST;
-    WHILE (i IS NOT NULL) LOOP
-      DBMS_OUTPUT.PUT_LINE(in_v_array(i));
-      i := in_v_array.PRIOR(i);
-    END LOOP;
-  
-  END display_reverse_v_array;
-
-end pt_work_with_v_array;
