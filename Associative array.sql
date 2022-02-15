72d610786cea792e4280860f00b699fa6e281986
Pashtet <paveltrfimv@gmail.com>
2022-02-15 09:15:26 +0300

    some refactoring

--- Associative array.sql
+++ Associative array.sql
@@ -6,76 +6,79 @@ declare
   a_array_test a_array;
   i            PLS_INTEGER;
 
-  PROCEDURE display_direct_a_array(in_a_array IN a_array) IS
+  PROCEDURE pl(in_text IN VARCHAR2) IS
   BEGIN
-    DBMS_OUTPUT.PUT_LINE('Display direct array:');
+    DBMS_OUTPUT.PUT_LINE(in_text);
+  END pl;
+
+  PROCEDURE display_direct(in_a_array IN a_array) IS
+  BEGIN
+    pl('Display direct array:');
     i := in_a_array.FIRST;
     WHILE (i IS NOT NULL) LOOP
-      DBMS_OUTPUT.PUT_LINE(in_a_array(i));
+      pl(in_a_array(i));
       i := in_a_array.NEXT(i);
     END LOOP;
-  END display_direct_a_array;
+  END display_direct;
 
-  PROCEDURE display_reverse_a_array(in_a_array IN a_array) IS
+  PROCEDURE display_reverse(in_a_array IN a_array) IS
   BEGIN
-    DBMS_OUTPUT.PUT_LINE('Display reverse array:');
+    pl('Display reverse array:');
     i := in_a_array.LAST;
     WHILE (i IS NOT NULL) LOOP
-      DBMS_OUTPUT.PUT_LINE(in_a_array(i));
+      pl(in_a_array(i));
       i := in_a_array.PRIOR(i);
     END LOOP;
-  END display_reverse_a_array;
+  END display_reverse;
 
 begin
 
-  DBMS_OUTPUT.PUT_LINE('Create assoсiative array from 1 to 10.');
+  pl('Create assoсiative array from 1 to 10.');
   FOR i IN 1 .. 10 LOOP
     a_array_test(i) := i;
   END LOOP;
 
-  display_direct_a_array(a_array_test);
+  display_direct(a_array_test);
 
-  display_reverse_a_array(a_array_test);
+  display_reverse(a_array_test);
 
-  DBMS_OUTPUT.PUT_LINE('Display number of elements a_array: ' ||
-                       a_array_test.COUNT);
+  pl('Display number of elements a_array: ' || a_array_test.COUNT);
 
-  DBMS_OUTPUT.PUT_LINE('Delete first element in a_array!');
+  pl('Delete first element in a_array!');
   a_array_test.DELETE(a_array_test.FIRST);
-  display_direct_a_array(a_array_test);
+  display_direct(a_array_test);
 
-  DBMS_OUTPUT.PUT_LINE('Is element 10 exist in a_array?');
+  pl('Is element 10 exist in a_array?');
   IF a_array_test.EXISTS(10) THEN
-    DBMS_OUTPUT.PUT_LINE('YES!');
+    pl('YES!');
   ELSE
-    DBMS_OUTPUT.PUT_LINE('No!');
+    pl('No!');
   END IF;
 
-  DBMS_OUTPUT.PUT_LINE('Delete last element in a_array!');
+  pl('Delete last element in a_array!');
   a_array_test.DELETE(a_array_test.LAST);
-  display_direct_a_array(a_array_test);
+  display_direct(a_array_test);
 
-  DBMS_OUTPUT.PUT_LINE('Is element 10 still exist in a_array?');
+  pl('Is element 10 still exist in a_array?');
   IF a_array_test.EXISTS(10) THEN
-    DBMS_OUTPUT.PUT_LINE('YES!');
+    pl('YES!');
   ELSE
-    DBMS_OUTPUT.PUT_LINE('No!');
+    pl('No!');
   END IF;
 
-  DBMS_OUTPUT.PUT_LINE('Display number of elements a_array: ' ||
-                       a_array_test.COUNT);
+  pl('Display number of elements a_array: ' || a_array_test.COUNT);
 
-  DBMS_OUTPUT.PUT_LINE('Add first element 10 in a_array');
+  pl('Add first element 10 in a_array');
   a_array_test(a_array_test.FIRST - 1) := 10;
-  display_direct_a_array(a_array_test);
-  
-    DBMS_OUTPUT.PUT_LINE('Add last element 1 in a_array');
+  display_direct(a_array_test);
+
+  pl('Add last element 1 in a_array');
   a_array_test(a_array_test.LAST + 1) := 1;
-  display_direct_a_array(a_array_test);
-  
+  display_direct(a_array_test);
+
   -- TRIM only to nested table or VARRAY
- /* DBMS_OUTPUT.PUT_LINE('Trim a_array');
+  /* pl('Trim a_array');
   a_array_test.TRIM;
-  display_direct_a_array(a_array_test);*/
-  
+  display_direct(a_array_test);*/
+
 end;
